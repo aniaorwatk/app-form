ffcc8a0d2ee0446e7633add9485cffd717d85e59
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-09-02 07:51:14 +0200

    cleared environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+import "./App.css";
+
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
