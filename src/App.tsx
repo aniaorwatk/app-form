b573fb352d1cda41389e5f8ff07bdfa34c52227e
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-09-02 09:08:59 +0200

    add header and create variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-
-import "./App.css";
+import Header from "./component/Header/Header";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
