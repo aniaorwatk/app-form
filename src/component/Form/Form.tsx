0afe22dda13bdcd29e6ddd6bf2caeb5cdc264059
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-09-04 17:32:59 +0200

    add button

--- src/component/Form/Form.tsx
+++ src/component/Form/Form.tsx
@@ -1,2 +1,26 @@
 import React from "react";
 import "./From.scss";
+
+const Form =()=>{
+
+    return(
+<main>
+<div>
+<form action="">
+
+
+    
+</form>
+
+
+
+
+
+</div>
+</main>
+    )
+
+
+}
+
+export default Form
